0f4a2c2610a9950b3851f37efbefc8e50c333c4b
Zakia Yahya <zyahya.msee15seecs@seecs.edu.pk>
2024-03-13 19:59:22 +0500

    Cluster provider should return nothing

--- parsl/providers/cluster_provider.py
+++ parsl/providers/cluster_provider.py
@@ -82,42 +82,42 @@ class ClusterProvider(ExecutionProvider):
         return self.channel.execute_wait(cmd, t)
 
     def _write_submit_script(self, template, script_filename, job_name, configs):
-        """Generate submit script and write it to a file.
+      """Generate submit script and write it to a file.
 
-        Args:
-              - template (string) : The template string to be used for the writing submit script
-              - script_filename (string) : Name of the submit script
-              - job_name (string) : job name
-              - configs (dict) : configs that get pushed into the template
+      Args:
+          - template (string) : The template string to be used for the writing submit script
+          - script_filename (string) : Name of the submit script
+          - job_name (string) : job name
+          - configs (dict) : configs that get pushed into the template
 
-        Returns:
-              - True: on success
+      Returns:
+          - True: None
 
-        Raises:
-              SchedulerMissingArgs : If template is missing args
-              ScriptPathError : Unable to write submit script out
-        """
+      Raises:
+          - SchedulerMissingArgs : If template is missing args
+          - ScriptPathError : Unable to write submit script out
+      """
 
-        try:
-            submit_script = Template(template).substitute(jobname=job_name, **configs)
-            with open(script_filename, 'w') as f:
-                f.write(submit_script)
+      try:
+        submit_script = Template(template).substitute(jobname=job_name, **configs)
+        with open(script_filename, 'w') as f:
+          f.write(submit_script)
 
-        except KeyError as e:
-            logger.error("Missing keys for submit script : %s", e)
-            raise SchedulerMissingArgs(e.args, self.label)
+      except KeyError as e:
+        logger.error("Missing keys for submit script : %s", e)
+        raise SchedulerMissingArgs(e.args, self.label)
 
-        except IOError as e:
-            logger.error("Failed writing to submit script: %s", script_filename)
-            raise ScriptPathError(script_filename, e)
-        except Exception as e:
-            print("Template : ", template)
-            print("Args : ", job_name)
-            print("Kwargs : ", configs)
-            logger.error("Uncategorized error: %s", e)
-            raise e
+      except IOError as e:
+        logger.error("Failed writing to submit script: %s", script_filename)
+        raise ScriptPathError(script_filename, e)
+      except Exception as e:
+        print("Template : ", template)
+        print("Args : ", job_name)
+        print("Kwargs : ", configs)
+        logger.error("Uncategorized error: %s", e)
+        raise e
 
-        return True
+      return None
 
     @abstractmethod
     def _status(self):
